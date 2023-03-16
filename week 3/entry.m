138c75847ba04f6afb193678ceb5ef58ec3ff3f7
ali.yuldashev <aliyuldashev880@gmail.com>
2023-03-16 09:36:26 +0900

    week3

---
+++ week 3/entry.m
@@ -0,0 +1,12 @@
+a = 4; // assigning new value to variable
+b = 17.8
+
+disp(); //displaying objects 
+
+squerRoot = sqrt(a + 12); // squer root of element and making operations
+
+rootElements = nthroot(b + 9.8,3); // 3th root of element
+
+disp(squerRoot);
+
+disp(rootElements)
---
+++ week 3/untitled.m
@@ -0,0 +1,22 @@
+a =4 ;
+b = 17.2;
+first = 5 * a;
+str3 = strcat('answer to first equation : ',num2str(first));
+disp(str3);
+
+second_1 = (b + 9.8) ^ (1/3);
+str3 = strcat('answer to second equation : ',num2str(second_1));
+disp(str3);
+% disp('answer of second equation : ');disp(second_1);
+% second_2 = nthroot(b + 9.8,3);
+
+third_1 = 10 * (5 * a + 16) ^ (1/2);
+str3 = strcat('answer to third equation : ',num2str(third_1));
+disp(str3);
+
+
+% disp('answer of second equation :');disp(third_1);
+
+% third_2 = 10 * nthroot(5 * a + 16,2);
+
+% third_3 = 10 * sqrt(5 * a + 16);
